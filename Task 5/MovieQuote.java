ce76749cbcb98d2a604164b83d2fe4fcc482833d
Ilhamw <31534077+Ilhamw@users.noreply.github.com>
2018-08-28 09:42:23 +0800

    my prac-1 soluion(2)

---
+++ Task 5/MovieQuote.java
@@ -0,0 +1,7 @@
+public class MovieQuote
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Let go of the past, and keep moving forward");
+    }
+}
---
+++ Task 5/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+public class MovieQuoteInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Let go of the past, and keep moving forward");
+        System.out.println("Meet The Robinsons");
+        System.out.println("Luis Robinson");
+        System.out.println("2007");
+    }
+}
